0e370e7260fdfc989b25f2d297a9e451303707e2
seerat <srizv2@sapient.com>
2017-04-10 11:33:13 +0530

    Comment removed

--- app/store/store.js
+++ app/store/store.js
@@ -1,6 +1,4 @@
 import {createStore, compse} from 'redux';
-//import {syncHistoryWithStore} from 'react-router-redux';
-//import {browserHistory} from 'react-router';
 import rootReducer from '../reducers/rootReducer';
 
 import Items from '../data/items';
@@ -8,6 +6,5 @@ import category from '../data/category';
 
 let defaultState = {Items, category};
 let store = createStore(rootReducer, defaultState);
-//export const history = syncHistoryWithStore(browserHistory, store);
+
 export default store;
-console.log(store);
